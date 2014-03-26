ad0e088152c77bc73828d4e692e884d63e376718
JamesNix <jamesfnix@gmail.com>
2014-03-26 16:20:21 -0500

    added new message alert

--- app/js/source/main.js
+++ app/js/source/main.js
@@ -1,4 +1,5 @@
-/* jshint camelcase:false */
+/* global Firebase:false */
+/* exported Firebase */
 
 (function(){
 
@@ -7,7 +8,40 @@
   $(document).ready(initialize);
 
   function initialize(){
+    $('#messageInput').keypress(pushData);
+    myDataRef.on('child_added', pullData);
+    $('#nameInput').focus();
   }
 
-  
+  var myDataRef = new Firebase('https://luminous-fire-3912.firebaseio.com/');
+
+  function pushData(e){
+    if (e.keyCode === 13){
+      var name = $('#nameInput').val();
+      var text = $('#messageInput').val();
+      myDataRef.push({name: name, text: text}); // This section takes input values and pushes as object to my data ref on Firebase
+      $('#messageInput').val('');
+    }
+  }
+
+  function pullData(snapshot){
+    var message = snapshot.val();
+    displayChatMessage(message.name, message.text);
+  }
+
+  function displayChatMessage(name, text){
+    $('<div/>').text(text).prepend($('<em/>').text(name+': ')).appendTo($('#messagesDiv'));
+    $('#messagesDiv')[0].scrollTop = $('#messagesDiv')[0].scrollHeight;
+    if(name !== $('#nameInput').val()){
+      document.getElementById('alert').play();  //$('#alert').trigger('play'); will also work here
+      //displayChatName(message.name);
+    }
+  }
+
+  // function displayChatName(name){
+  //   if(name)
+  // }
+
+
+
 })();
--- app/js/source/test.js
+++ app/js/source/test.js
@@ -1,8 +1,9 @@
-/* global test:false, ok:false */
+/* global Firebase, test, ok:false */
 
 'use strict';
 
-test('template test', function(){
-  ok( 1 === 1, '1 should equal 1');
-});
+test('Firebase#new', function(){
+  var f1 = new Firebase('https://luminous-fire-3912.firebaseio.com/');
 
+  ok(f1 instanceof Firebase, 'f1 should be an instance of Firebase');
+});
