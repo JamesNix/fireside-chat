3317be8fdd49fc05c2c379983c435f3deb168352
JamesNix <jamesfnix@gmail.com>
2014-03-28 18:16:20 -0500

    multi user functionality

--- app/js/source/main.js
+++ app/js/source/main.js
@@ -12,54 +12,72 @@
     myDataRef.on('child_added', pullData);
     $('#nameInput').focus();
     $('#reset').click(resetChat);
-    document.onload(resetChat);
-    //weatherRef.child('temperature').on('value', getWeather());
+    $('#fire').click(makeFire);
   }
 
   var myDataRef = new Firebase('https://luminous-fire-3912.firebaseio.com/');
-  //var weatherRef = new Firebase('https://publicdata-weather.firebaseio.com/nashville/currently');
+  //var localUser;
+  var nameIndex;
+  var chatNames = [];
 
   function pushData(e){
     if (e.keyCode === 13){
+    //localUser = $('#nameInput').val();  // Find a better way to do this
       var name = $('#nameInput').val();
       var text = $('#messageInput').val();
 
       myDataRef.push({name: name, text: text}); // This section takes input values and pushes as object to my data ref on Firebase
       $('#messageInput').val('');
       $('#messageInput').focus();
-      $('#nameInput').attr('disabled', 'true'); //prevents changing name mid-chat
-      chatNames.push(name);
+      $('#nameInput').attr('disabled', 'true'); // Prevents changing name mid-chat
     }
   }
 
   function pullData(snapshot){
     var message = snapshot.val();
+
     displayChatMessage(message.name, message.text);
 
-    chatNames.push(message.name); //experimental
+    console.log(message.name);
+    console.log(chatNames);
   }
 
   function displayChatMessage(name, text){
+    debugger;
     var $div = $('<div/>').addClass('messages');
     var $clear = $('<div/>').addClass('clear');
+    var localUser = $('#nameInput').val();
+    var chatColors = ['lightblue', 'red', 'lightgreen', 'yellow', 'deeppink', 'chartreuse', 'goldenrod', 'lightsalmon', 'deeppink'];
 
-    if(name !== $('#nameInput').val()){
-      document.getElementById('alert').play();
-      $div.addClass('foreign');
-      $div.text(text).prepend($('<em/>').text(name+':  ')).appendTo($('#messagesDiv'));
-      $clear.appendTo($('#messagesDiv'));
-      $('#messagesDiv')[0].scrollTop = $('#messagesDiv')[0].scrollHeight;
-    } else {
-      $div.addClass('local');
-      $div.text(text).prepend($('<em/>').text(name+':  ')).appendTo($('#messagesDiv'));
-      $clear.appendTo($('#messagesDiv'));
-      $('#messagesDiv')[0].scrollTop = $('#messagesDiv')[0].scrollHeight;
+
+    if($.inArray(name, chatNames) === -1){ //Pushes name to array if not there
+      chatNames.push(name);
+      nameIndex++;
     }
+
+    if(name === localUser){
+      $div.addClass('local');
+    } else {
+      document.getElementById('alert').play(); // Plays alert on incoming message
+      $div.addClass('foreign');
+    }
+
+    $div.text(text).prepend($('<em/>').text(name+':  ')).appendTo($('#messagesDiv'));
+    $div.css('background-color', chatColors[chatNames.indexOf(name)]);
+    $clear.appendTo($('#messagesDiv'));
+    $('#messagesDiv')[0].scrollTop = $('#messagesDiv')[0].scrollHeight;
   }
 
-  // function getWeather(snapshot){
-  //   console.log('Temperature is currently ' + snapshot.val());
-  // }
+  function makeFire() {
+    $('body').toggleClass('fire');
+    if($('#fire').text('Go to hell')){
+      $('#fire').text('');
+      $('#fire').text('... and back');
+    } else {
+      $('#fire').text('');
+      $('#fire').text('Go to hell');
+    }
+  }
 
   function resetChat(){
     $('#messagesDiv').empty();
