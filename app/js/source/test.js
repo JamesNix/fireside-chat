15d0472e666516d9ac9fa6c48f48a71e6a4334ba
James Nix <jamesfnix@gmail.com>
2014-11-20 11:39:17 -0600

    Delete test.js

--- app/js/source/test.js
+++
@@ -1,9 +0,0 @@
-/* global Firebase, test, ok:false */
-
-'use strict';
-
-test('Firebase#new', function(){
-  var f1 = new Firebase('https://luminous-fire-3912.firebaseio.com/');
-
-  ok(f1 instanceof Firebase, 'f1 should be an instance of Firebase');
-});
